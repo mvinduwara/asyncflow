cde282d158600d955702a05786f55d2a72b0a6e4
mvinduwara <mvinduwara@gmail.com>
2026-05-22 12:48:20 +0530

    new commit

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -1,10 +1,9 @@
-// apps/backend/prisma.config.ts
 import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL'), // The CLI pulls the URL from here now
+    url: env('DATABASE_URL'), 
   },
 });
